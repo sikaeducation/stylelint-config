9d6e344c36f57a0ff819aef80d3a2efbfa830fe6
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-06 01:34:01 -0600

    Fix error

--- index.js
+++ index.js
@@ -51,6 +51,13 @@ module.exports = {
 	},
 	"reportInvalidScopeDisables": true,
 	"reportNeedlessDisables": true,
-	"ignoreFiles": ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx", ".html"],
+	"ignoreFiles": [
+		"node_modules/**",
+		"**/*.js",
+		"**/*.jsx",
+		"**/*.ts",
+		"**/*.tsx",
+		"**/*.html",
+	],
 }
 
