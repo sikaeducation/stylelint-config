9e15b9f8ed999d357b3f4b30d8db9c624f685a65
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-07 02:32:21 -0600

    Stable

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 module.exports = {
-	extends: [
-		"stylelint-config-standard-scss",
-		"stylelint-config-prettier-scss",
-		"@ronilaukkarinen/stylelint-a11y/recommended",
-		"stylelint-config-hudochenkov/order",
-	],
-	ignoreFiles: [
-		"node_modules/**",
-		"**/*.js",
-		"**/*.cjs",
-		"**/*.mjs",
-		"**/*.jsx",
-		"**/*.ts",
-		"**/*.tsx",
-		"**/*.html",
-	],
-	plugins: [
-		"stylelint-color-format",
-		"stylelint-use-nesting",
-		"stylelint-plugin-defensive-css",
-		"stylelint-plugin-logical-css",
-	],
-	rules: {
-		"selector-class-pattern": null,
-		"selector-id-pattern": null,
-		"block-no-empty": null,
-		"color-format/format": {
-			format: "hsla",
-		},
-		"csstools/use-nesting": "always",
-		"plugin/use-defensive-css": [
-			true,
-			{
-				"accidental-hover": true,
-			},
-			{
-				"background-repeat": true,
-			},
-			{
-				"flex-wrapping": true,
-			},
-			{
-				"scroll-chaining": true,
-			},
-			{
-				"vendor-prefix-grouping": true,
-			},
-		],
-		"plugin/use-logical-properties-and-values": [
-			true,
-			{
-				severity: "warning",
-			},
-		],
-		"plugin/use-logical-units": [
-			true,
-			{
-				severity: "warning",
-			},
-		],
-	},
-	reportInvalidScopeDisables: true,
-	reportNeedlessDisables: true,
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-standard-html",
+    "@ronilaukkarinen/stylelint-a11y/recommended",
+    "stylelint-config-hudochenkov/order",
+  ],
+  ignoreFiles: [
+    "node_modules/**",
+    "**/*.js",
+    "**/*.cjs",
+    "**/*.mjs",
+    "**/*.jsx",
+    "**/*.ts",
+    "**/*.tsx",
+    "**/*.html",
+  ],
+  plugins: [
+    "stylelint-color-format",
+    "stylelint-use-nesting",
+    "stylelint-plugin-defensive-css",
+    "stylelint-plugin-logical-css",
+  ],
+  rules: {
+    "selector-class-pattern": null,
+    "selector-id-pattern": null,
+    "block-no-empty": null,
+    "color-format/format": {
+      format: "hsla",
+    },
+    "csstools/use-nesting": "always",
+    "plugin/use-defensive-css": [
+      true,
+      {
+        "accidental-hover": true,
+      },
+      {
+        "background-repeat": true,
+      },
+      {
+        "flex-wrapping": true,
+      },
+      {
+        "scroll-chaining": true,
+      },
+      {
+        "vendor-prefix-grouping": true,
+      },
+    ],
+    "plugin/use-logical-properties-and-values": [
+      true,
+      {
+        severity: "warning",
+      },
+    ],
+    "plugin/use-logical-units": [
+      true,
+      {
+        severity: "warning",
+      },
+    ],
+  },
+  reportInvalidScopeDisables: true,
+  reportNeedlessDisables: true,
 };
