3f2c2b94792a25306408db91675a559d36e52ffa
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-06 20:02:03 -0600

    Update to gitignore syntax

--- index.js
+++ index.js
@@ -1,55 +1,64 @@
 module.exports = {
-  extends: [
-    "stylelint-config-standard-scss",
-    "@ronilaukkarinen/stylelint-a11y/recommended",
-    "stylelint-config-hudochenkov/order",
-  ],
-  plugins: [
-    "stylelint-color-format",
-    "stylelint-use-nesting",
-    "stylelint-plugin-defensive-css",
-    "stylelint-plugin-logical-css",
-  ],
-  rules: {
-    "selector-class-pattern": null,
-    "selector-id-pattern": null,
-    "block-no-empty": null,
-    "color-format/format": {
-      format: "hsla",
-    },
-    "csstools/use-nesting": "always",
-    "plugin/use-defensive-css": [
-      true,
-      {
-        "accidental-hover": true,
-      },
-      {
-        "background-repeat": true,
-      },
-      {
-        "flex-wrapping": true,
-      },
-      {
-        "scroll-chaining": true,
-      },
-      {
-        "vendor-prefix-grouping": true,
-      },
-    ],
-    "plugin/use-logical-properties-and-values": [
-      true,
-      {
-        severity: "warning",
-      },
-    ],
-    "plugin/use-logical-units": [
-      true,
-      {
-        severity: "warning",
-      },
-    ],
-  },
-  reportInvalidScopeDisables: true,
-  reportNeedlessDisables: true,
-  ignoreFiles: ["node_modules/**", "**/*.{js,cjs,mjs,jsx,ts,tsx,html}"],
+	extends: [
+		"stylelint-config-standard-scss",
+		"@ronilaukkarinen/stylelint-a11y/recommended",
+		"stylelint-config-hudochenkov/order",
+	],
+	plugins: [
+		"stylelint-color-format",
+		"stylelint-use-nesting",
+		"stylelint-plugin-defensive-css",
+		"stylelint-plugin-logical-css",
+	],
+	rules: {
+		"selector-class-pattern": null,
+		"selector-id-pattern": null,
+		"block-no-empty": null,
+		"color-format/format": {
+			format: "hsla",
+		},
+		"csstools/use-nesting": "always",
+		"plugin/use-defensive-css": [
+			true,
+			{
+				"accidental-hover": true,
+			},
+			{
+				"background-repeat": true,
+			},
+			{
+				"flex-wrapping": true,
+			},
+			{
+				"scroll-chaining": true,
+			},
+			{
+				"vendor-prefix-grouping": true,
+			},
+		],
+		"plugin/use-logical-properties-and-values": [
+			true,
+			{
+				severity: "warning",
+			},
+		],
+		"plugin/use-logical-units": [
+			true,
+			{
+				severity: "warning",
+			},
+		],
+	},
+	reportInvalidScopeDisables: true,
+	reportNeedlessDisables: true,
+	ignoreFiles: [
+		"node_modules/**",
+		"**/*.js",
+		"**/*.cjs",
+		"**/*.mjs",
+		"**/*.jsx",
+		"**/*.ts",
+		"**/*.tsx",
+		"**/*.html",
+	],
 };
